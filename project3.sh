9d52a0ea8745628faa8fa64c24cc497770c3c9d4
samfalmar <a15samfalmar@iam.cat>
2019-05-13 17:25:59 +0200

    UserRoot

--- project3.sh
+++ project3.sh
@@ -1,1 +1,9 @@
 #! /bin/bash
+clear
+#Make sure the script is being executed with superuser privileges
+	if ! [ $(id -u) = 0 ];
+	then
+		echo "No sou root"
+		echo "Executeu-lo amb privilegis"
+	exit 1
+	fi
